fb2dd4869b77721b4c325709f7646d6d1d0047bc
Tom <tomdesvignes031@gmail.com>
2023-12-15 11:46:21 +0100

    maj

--- index.js
+++ index.js
@@ -73,10 +73,13 @@ app.get("/api/data", async (req, res) => {
 
 app.get("/api/healthcheck/mongo", async (req, res) => {
     try {
-    
-        res.json({ message: client.isConnected() });
+        if (!client.isConnected()) {
+            res.json({ message: "Connexion à la base de données échouée" });
+        }
+        else
+            res.json({ message: true });
     } catch (error) {
-        res.status(500).json({error, test: "test"});
+        res.status(500).json({ error, test: "test" });
     }
 })
 
