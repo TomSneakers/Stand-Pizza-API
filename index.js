cf405a46116d60ee3cdc343f4e334ff3790eb7e8
Tom <tomdesvignes031@gmail.com>
2024-03-27 13:34:13 +0100

    maj

--- index.js
+++ index.js
@@ -24,8 +24,9 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+const uri = "mongodb+srv://tomdesvignes031:W1q5VQtTqcTdJEHK@@stand-pizza.d2y0rsl.mongodb.net/?retryWrites=true&w=majority&appName=stand-pizza";
 
-const uri = "mongodb+srv://tomdesvignes031:W1q5VQtTqcTdJEHK@stand-pizza.d2y0rsl.mongodb.net/";
+// const uri = "mongodb+srv://tomdesvignes031:W1q5VQtTqcTdJEHK@stand-pizza.d2y0rsl.mongodb.net/";
 // const uri = "mongodb+srv://tomdesvignes031:3BAq8uH*nkCx8N#@stand-pizza.d2y0rsl.mongodb.net/";
 
 const client = new MongoClient(uri, {});
