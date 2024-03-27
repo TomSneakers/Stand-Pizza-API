60d0b212a8eecc86b93fac9aefe7ee604a992265
Tom <tomdesvignes031@gmail.com>
2024-03-27 21:17:30 +0100

    maj

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-const express = require("express");
-const { MongoClient } = require("mongodb");
-const { ObjectId } = require("mongodb");
-const cors = require("cors");
-const multer = require("multer");
-const path = require("path");
-
-const app = express();
+const multer = require('multer');
+const path = require('path');
 const upload = multer({
     dest: 'uploads/',
     storage: multer.diskStorage({
@@ -19,51 +13,59 @@ const upload = multer({
     }),
 });
 
+const { MongoClient } = require("mongodb");
+const { ObjectId } = require("mongodb");
+const cors = require("cors");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const express = require("express");
+const app = express();
+
 app.use(express.json());
 app.use(cors());
-
 const uri = "mongodb+srv://tomdesvignes031:wh7Emtt4chDKIaJq@stand-pizza.d2y0rsl.mongodb.net/";
+
+
 
 const client = new MongoClient(uri, {});
 
 let database;
-let ordersCollection;
-let pizzasCollection;
+let collection;
+let pizzaCollection;
 
-const connectToDatabase = async () => {
+const startServer = async () => {
     await client.connect();
-    console.log("Connected to the database successfully");
+    console.log("Connexion à la base de données établie avec succès");
     database = client.db("Stand-pizza");
-    ordersCollection = database.collection("commandes-pizza");
-    pizzasCollection = database.collection("pizzas");
+    collection = database.collection("commandes-pizza");
+
+    pizzaCollection = database.collection("pizzas");
+
+    const changeStream = collection.watch();
+
+    changeStream.on("change", (change) => {
+        io.emit("dataChange", change);
+    });
 };
 
-connectToDatabase().catch(console.error);
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "https://stand-pizza.online/",
+        methods: ["GET", "POST"],
+        credentials: true,
+        transports: ['websocket', 'polling'],
+    },
+});
 
-// Middleware pour vérifier si la base de données est connectée
-const checkDatabaseConnection = (req, res, next) => {
-    if (database && ordersCollection && pizzasCollection) {
-        next();
-    } else {
-        res.status(500).json({ error: "Database connection error" });
-    }
-};
 
-// Middleware pour gérer les erreurs
-const errorHandler = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ error: "Something went wrong!" });
-};
-
-app.use(checkDatabaseConnection);
-
-// Routes
 
 app.get("/api/uploads/:filename", async (req, res) => {
     try {
         const filename = req.params.filename;
         res.header["Content-Type"] = "image/png";
-        res.sendFile(path.join(__dirname, "/uploads/", filename));
+        res.sendFile(__dirname + "/uploads/" + filename);
     } catch (error) {
         console.error("Erreur lors de la récupération du fichier :", error);
         res.status(500).json({ error: "Erreur lors de la récupération du fichier" });
@@ -73,7 +75,7 @@ app.get("/api/uploads/:filename", async (req, res) => {
 app.get("/api/data", async (req, res) => {
     try {
         const query = {};
-        const result = await ordersCollection.find(query).toArray();
+        const result = await collection.find(query).toArray();
         res.json(result);
     } catch (error) {
         console.error("Erreur lors de la récupération des données :", error);
@@ -92,7 +94,7 @@ app.post("/api/place-order", async (req, res) => {
             orderItems: orderItems,
         };
 
-        await ordersCollection.insertOne(orderDocument);
+        await collection.insertOne(orderDocument);
 
         res.status(200).json({ message: "Commande enregistrée avec succès" });
     } catch (error) {
@@ -103,7 +105,7 @@ app.post("/api/place-order", async (req, res) => {
 
 app.get("/api/pizzas", async (req, res) => {
     try {
-        const pizzas = await pizzasCollection.find({}).toArray();
+        const pizzas = await pizzaCollection.find({}).toArray();
         res.json(pizzas);
     } catch (error) {
         console.error("Erreur lors de la récupération des pizzas :", error);
@@ -126,7 +128,7 @@ app.post('/api/add-pizza', upload.single('logo'), async (req, res) => {
             logo: logo,
         };
 
-        await pizzasCollection.insertOne(pizzaDocument);
+        await pizzaCollection.insertOne(pizzaDocument);
 
         res.status(200).json({ message: 'Pizza ajoutée avec succès' });
     } catch (error) {
@@ -137,7 +139,7 @@ app.post('/api/add-pizza', upload.single('logo'), async (req, res) => {
 
 app.get("/api/pizza-logos", async (req, res) => {
     try {
-        const logos = await pizzasCollection.find({}).toArray();
+        const logos = await pizzaCollection.find({}).toArray();
         res.json(logos);
     } catch (error) {
         console.error("Erreur lors de la récupération des logos de pizzas :", error);
@@ -151,7 +153,7 @@ app.delete("/api/pizzas/:id", async (req, res) => {
     try {
         const pizzaId = req.params.id;
 
-        const result = await pizzasCollection.deleteOne({ _id: new ObjectId(pizzaId) });
+        const result = await pizzaCollection.deleteOne({ _id: new ObjectId(pizzaId) });
 
         if (result.deletedCount === 0) {
             return res.status(404).json({ error: "Pizza non trouvée" });
@@ -166,7 +168,7 @@ app.delete("/api/pizzas/:id", async (req, res) => {
 
 app.delete("/api/delete-all-orders", async (req, res) => {
     try {
-        await ordersCollection.deleteMany({});
+        await collection.deleteMany({});
 
         res.status(200).json({ message: "Toutes les commandes ont été supprimées avec succès" });
     } catch (error) {
@@ -180,7 +182,7 @@ app.delete("/api/delete-order/:id", async (req, res) => {
     console.log("Deleting order with ID:", orderId);
 
     // Ajouter la logique pour vérifier si la commande existe avant de la supprimer
-    const result = await ordersCollection.deleteOne({ _id: new ObjectId(orderId) });
+    const result = await collection.deleteOne({ _id: new ObjectId(orderId) });
 
     if (result.deletedCount === 0) {
         return res.status(404).json({ error: "Command not found" });
@@ -189,28 +191,24 @@ app.delete("/api/delete-order/:id", async (req, res) => {
     res.status(200).json({ message: "Order deleted successfully" });
 });
 
+
 app.put("/api/toggle-payment/:id", async (req, res) => {
     try {
         const orderId = req.params.id;
         const { isPaid } = req.body;
 
-        await ordersCollection.updateOne(
+        await collection.updateOne(
             { _id: new ObjectId(orderId) },
             { $set: { isPaid } }
         );
 
-        res.status(200).json({ message: "Statut de paiement mis à jour avecs uccès" });
+        res.status(200).json({ message: "Statut de paiement mis à jour avec succès" });
     } catch (error) {
         console.error("Erreur lors de la mise à jour du statut de paiement :", error);
         res.status(500).json({ error: "Erreur lors de la mise à jour du statut de paiement" });
     }
 });
 
-app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
 
 module.exports = app;
